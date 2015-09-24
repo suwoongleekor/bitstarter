e23d6219a0906ce5b9a082274dcf1074e70aa555
Ubuntu <ubuntu@ip-172-31-18-191.us-west-2.compute.internal>
2015-09-24 00:42:31 +0000

    css index.html

---
+++ web.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+var fs=require('fs');
+app.get('/', function(request, response) {
+  
+  var index=fs.readFileSync('index.html');
+  var string=index.toString('utf-8');
+  response.send(string);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
